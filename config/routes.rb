fef126e75e1a9f306cb804cada93ec7a81a16231
mskk0803 <toyama2ura@gmail.com>
2024-11-03 14:29:38 +0900

    ルート、ログイン・ログアウトじリダイレクト先をgame#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'user_sessions/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "games#index"
-  # 後で変更
-  root "user_sessions#new"
-  resources :user_sessions, only: %i[new create]
-  resources :articles, only: %i[new create show edit update destroy],shallow: true do
+  root "games#index"
+  resources :games, only: %i[index new create show edit update destroy],shallow: true do
   end
 end
