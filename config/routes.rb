ed743e6cd7b2908ddbe18f3344bb58e6cbf7b679
mskk0803 <toyama2ura@gmail.com>
2024-11-03 14:10:21 +0900

    deviseを用いたログイン機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "games#index"
-  # get "/login" =>
+  # 後で変更
+  root "user_sessions#new"
   resources :user_sessions, only: %i[new create]
   resources :articles, only: %i[new create show edit update destroy],shallow: true do
   end
--- db/migrate/20241102053748_rename_user_name_column_to_user.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserNameColumnToUser < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :user_name, :email
-  end
-end
