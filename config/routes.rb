49335835de4a51f9d45dcd9b36548cc3472cf980
mskk0803 <toyama2ura@gmail.com>
2024-11-06 13:52:37 +0900

    アセットパイプラインの見直し

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "games#index"
-  resources :games, only: %i[index new create show edit update destroy],shallow: true do
-  end
+  resources :games, only: %i[index new create show edit update destroy]
 end
