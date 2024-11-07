c9d0fb03037330a003be249ada1d05ec1cefc1fa
mskk0803 <toyama2ura@gmail.com>
2024-11-07 16:07:22 +0900

    タグテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_04_104416) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_07_070515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_04_104416) do
     t.integer "time"
     t.integer "dificullty"
     t.integer "space"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
